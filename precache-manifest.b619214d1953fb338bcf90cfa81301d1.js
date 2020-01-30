a5ff450118bbe218c26914c1ce80b45814afe760
Fares Khiary <fares.khiary@talan.com>
2020-01-30 11:52:01 +0100

    Updates

---
+++ precache-manifest.b619214d1953fb338bcf90cfa81301d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b725e2ee5697c11d17a3a24a585e9a53",
+    "url": "/react-memory-app/index.html"
+  },
+  {
+    "revision": "997130623f5e6109f7e8",
+    "url": "/react-memory-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "415ddd543540244792d1",
+    "url": "/react-memory-app/static/js/2.178d8dcf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-memory-app/static/js/2.178d8dcf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "997130623f5e6109f7e8",
+    "url": "/react-memory-app/static/js/main.ff2085f1.chunk.js"
+  },
+  {
+    "revision": "801dd97e497c4840d392",
+    "url": "/react-memory-app/static/js/runtime-main.98513192.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-memory-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
