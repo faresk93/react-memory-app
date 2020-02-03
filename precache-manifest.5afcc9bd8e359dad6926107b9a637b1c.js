394a79ba6fb618cb028b0e1d56e69276d2d6a056
Fares Khiary <fares.khiary@talan.com>
2020-02-03 12:19:10 +0100

    Updates

---
+++ precache-manifest.5afcc9bd8e359dad6926107b9a637b1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f32a6e6cd85a7f02df6f5d474e17cb5",
+    "url": "/react-memory-app/index.html"
+  },
+  {
+    "revision": "9d0f8214c54deac50a9e",
+    "url": "/react-memory-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "e987de7628e1073be64c",
+    "url": "/react-memory-app/static/css/main.f8ca65c4.chunk.css"
+  },
+  {
+    "revision": "9d0f8214c54deac50a9e",
+    "url": "/react-memory-app/static/js/2.2a14d12d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-memory-app/static/js/2.2a14d12d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e987de7628e1073be64c",
+    "url": "/react-memory-app/static/js/main.35fbe71d.chunk.js"
+  },
+  {
+    "revision": "801dd97e497c4840d392",
+    "url": "/react-memory-app/static/js/runtime-main.98513192.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-memory-app/precache-manifest.5ac817d69089ae3d4e7856e5acaf5509.js"
+  "/react-memory-app/precache-manifest.5afcc9bd8e359dad6926107b9a637b1c.js"
 );
 
 self.addEventListener('message', (event) => {
